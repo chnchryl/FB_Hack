e5123efba2d85ef21172f65405c4a60f591354da
WenquanZou <wz1816@ic.ac.uk>
2018-02-03 17:01:08 +0000

    Deploy wordfrequency and plotly in app to be ready to use

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const app = express()
+const plotly = require('plotly')("WenquanZou", "hmjKVkVTlnhDioGsIVkL")
 
 const PORT = process.env.PORT || 5000
+
+const Freq = require('wordfrequenter')
+
+const test = 'this is a cool test string this is cool cool cool'
+
+const wf = new Freq(test.split(' '))
+
+wf.set('string')
+console.dir(wf.get('cool'))
+console.dir(wf.list())
 
 app.get('/', (req, res) => {
   console.log(__dirname)
   res.sendFile(__dirname + '/index.html')
 })
+
+var data = [{x:[0,1,2], y:[3,2,1], type: 'bar'}];
+var layout = {fileopt : "overwrite", filename : "simple-node-example"};
+
+plotly.plot(data, layout, function (err, msg) {
+	if (err) return console.log(err);
+	console.log(msg);
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
